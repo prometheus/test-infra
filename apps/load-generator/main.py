d36c79f4c2f414af5666e3c83915c9ca4a2ff41a
Fabian Reinartz <fab.reinartz@gmail.com>
2017-03-07 15:56:35 +0100

    Refactor scaler to accomodate querier in same script

--- apps/load-generator/main.py
+++ apps/load-generator/main.py
@@ -2,53 +2,90 @@
 
 import json
 import os
-import requests
-import schedule
 import time
+import sys
+import requests
 import yaml
 
-kubeToken = None
-verify = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-kubernetesServiceHostUrl = os.environ['KUBERNETES_SERVICE_HOST']
-kubernetesPort443TCPPort = os.environ['KUBERNETES_PORT_443_TCP_PORT']
-url = 'https://' + kubernetesServiceHostUrl + ':' + kubernetesPort443TCPPort
-urlDeployments = url + '/apis/extensions/v1beta1/namespaces/default/deployments/'
-urlConfigMaps = url + '/api/v1/namespaces/default/configmaps/'
-prevScalingUp = True
-config = None
+ca_cert_path = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 
-with open("/var/run/secrets/kubernetes.io/serviceaccount/token", 'r') as f:
-    kubeToken = f.read()
-headers = {'Content-type': 'application/json', 'Authorization': 'Bearer ' + kubeToken}
+def deployment_path(url, depl):
+    return '%s/apis/extensions/v1beta1/namespaces/default/deployments/%s' % (url, depl)
 
-def loadConfig():
-    print("Updating k8s-simulator-config")
-    global headers
-    global verify
-    global config
-    configMap = requests.get((urlConfigMaps + 'k8s-simulator-config'), headers=headers, verify=verify).json()
-    config = yaml.load(configMap["data"]["k8s-simulator-config.yml"])
-    print("Updated k8s-simulator-config")
+def configmap_path(url, cm):
+    return '%s/api/v1/namespaces/default/configmaps/%s' % (url, cm)
 
-def scaleDeployment(amountInstances):
-    global headers
-    global verify
-    fakeWebserverDeploy = requests.get((urlDeployments + config["name"]), headers=headers, verify=verify).json()
-    print("Scaling " + config["name"] + " to: " + str(amountInstances))
-    fakeWebserverDeploy["spec"]["replicas"] = amountInstances
-    requests.put(urlDeployments + "fake-webserver/", data=json.dumps(fakeWebserverDeploy), headers=headers, verify=verify)
+class Scaler(object):
+    """
+    Scaler periodically scales a deployment down a minimum number of replicas
+    and back up again.
+    """
+    def __init__(self, cfg, url, req_kwargs):
+        self.interval = int(cfg["intervalMinutes"]) * 60
+        self.deployment = cfg["name"]
+        self.low = cfg["low"]
+        self.high = cfg["high"]
+        self.url = url
+        self.req_kwargs = req_kwargs
 
-def alternateUpDownScaling():
-    global prevScalingUp
-    if prevScalingUp:
-        scaleDeployment(config["low"])
+    def run(self):
+        while True:
+            time.sleep(self.interval)
+
+            print("scaling deployment %s" % self.deployment)
+
+            self.scale(self.low)
+            time.sleep(30)
+            self.scale(self.high)
+
+    def scale(self, n):
+        p = deployment_path(self.url, self.deployment)
+
+        resp = requests.get(p, **self.req_kwargs).json()
+        resp["spec"]["replicas"] = n
+
+        requests.put(p, data=json.dumps(resp), **self.req_kwargs)
+
+
+class Querier(object):
+    """
+    Querier launches groups of queries against a set of Prometheus services.
+    """
+    def __init__(self, cfg, url, req_kwargs):
+        pass
+
+    def run(self):
+        pass
+
+
+
+def main():
+    if len(sys.argv) != 2 or sys.argv[1] not in ["scaler", "querier"]:
+        print("unexpected arguments")
+        print("usage: <load_generator> <scaler|querier>")
+        exit(2)
+
+    host = os.environ.get('KUBERNETES_SERVICE_HOST')
+    port = os.environ.get('KUBERNETES_PORT_443_TCP_PORT')
+    url = 'https://%s:%s' % (host, port)
+
+    kube_token = open("/var/run/secrets/kubernetes.io/serviceaccount/token", 'r').read()
+
+    req_kwargs = {
+        'headers': {'Content-type': 'application/json', 'Authorization': 'Bearer ' + kube_token},
+        'verify': ca_cert_path,
+    }
+
+    config_map = requests.get(configmap_path(url, 'prometheus-load-generator'), **req_kwargs).json()
+    config = yaml.load(config_map["data"]["config.json"])
+
+    print("loaded configuration")
+
+    if sys.argv[1] == "scaler":
+        Scaler(config["scaler"], url, req_kwargs).run()
     else:
-        scaleDeployment(config["high"])
-    prevScalingUp = not prevScalingUp
+        Querier(config["querier"], url, req_kwargs).run()
 
-loadConfig()
-schedule.every(config["intervalMinutes"]).minutes.do(alternateUpDownScaling)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    main()
