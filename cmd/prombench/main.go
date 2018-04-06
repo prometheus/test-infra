8936ac51ceab49aa92084b8e466a7709bbea6a89
Krasi Georgiev <krasi.root@gmail.com>
2018-04-06 11:12:32 +0300

    refactored to use the GRPC
    the package `cloud.google.com/go/container/apiv1` is more complete
    the http portion is not complete so switched to using the grpc
    should be able to use same package to apply manifest which
    wasn't possible with the official `api/container`
    
    Signed-off-by: Krasi Georgiev <kgeorgie@redhat.com>

--- cmd/prombench/main.go
+++ cmd/prombench/main.go
@@ -10,12 +10,6 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-type k8sGKECluster interface {
-	Create(*kingpin.ParseContext) error
-	// Delete()
-	// Scale()
-}
-
 func main() {
 
 	app := kingpin.New(filepath.Base(os.Args[0]), "The Prometheus benchmarking tool")
@@ -23,10 +17,11 @@ func main() {
 
 	g := &gke.Cluster{}
 	k8sGKE := app.Command("gke", "using the google container engine provider").Action(g.New)
-	k8sGKE.Flag("project", "the project name for the cluster").Default("prometheus").StringVar(&g.ProjectID)
-	k8sGKE.Flag("zone", "the zone for the cluster").Default("europe-west1-b").StringVar(&g.Zone)
-	k8sGKE.Flag("name", "the cluster name").Default("prombench").StringVar(&g.Name)
-	k8sGKE.Flag("nodeCount", "the total number of cluster nodes").Default("1").Int64Var(&g.NodeCount)
+	k8sGKE.Flag("project", "project name for the cluster").Default("prometheus").StringVar(&g.ProjectID)
+	k8sGKE.Flag("zone", "zone for the cluster").Default("europe-west1-b").StringVar(&g.Zone)
+	k8sGKE.Flag("name", "cluster name").Default("prombench").StringVar(&g.Name)
+	k8sGKE.Flag("nodeCount", "total number of cluster nodes").Default("1").Int32Var(&g.NodeCount)
+	k8sGKE.Flag("dashboard", "enable the dashboard").Default("false").BoolVar(&g.Dashboard)
 	k8sGKE.Command("create", "create a new k8sGKE cluster").Action(g.Create)
 	k8sGKE.Command("delete", "delete a k8sGKE cluster").Action(g.Delete)
 	k8sGKE.Command("list", "list k8sGKE clusters").Action(g.List)
