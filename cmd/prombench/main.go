9506349f5f62b239660ee113640073a9195e9419
Krasi Georgiev <krasi.root@gmail.com>
2018-04-08 09:58:47 +0300

    refactor , cleanup and implemented deployment apply
    Signed-off-by: Krasi Georgiev <kgeorgie@redhat.com>

--- cmd/prombench/main.go
+++ cmd/prombench/main.go
@@ -15,16 +15,24 @@ func main() {
 	app := kingpin.New(filepath.Base(os.Args[0]), "The Prometheus benchmarking tool")
 	app.HelpFlag.Short('h')
 
-	g := &gke.Cluster{}
-	k8sGKE := app.Command("gke", "using the google container engine provider").Action(g.New)
+	g := &gke.GKE{}
+	k8sGKE := app.Command("gke", "using the google container engine provider").Action(g.NewGKEClient)
 	k8sGKE.Flag("project", "project name for the cluster").Default("prometheus").StringVar(&g.ProjectID)
 	k8sGKE.Flag("zone", "zone for the cluster").Default("europe-west1-b").StringVar(&g.Zone)
 	k8sGKE.Flag("name", "cluster name").Default("prombench").StringVar(&g.Name)
-	k8sGKE.Flag("nodeCount", "total number of cluster nodes").Default("1").Int32Var(&g.NodeCount)
-	k8sGKE.Flag("dashboard", "enable the dashboard").Default("false").BoolVar(&g.Dashboard)
-	k8sGKE.Command("create", "create a new k8sGKE cluster").Action(g.Create)
-	k8sGKE.Command("delete", "delete a k8sGKE cluster").Action(g.Delete)
-	k8sGKE.Command("list", "list k8sGKE clusters").Action(g.List)
+	k8sGKE.Flag("nodeCount", "total number of k8s cluster nodes").Default("1").Int32Var(&g.NodeCount)
+	k8sGKE.Flag("dashboard", "enable the k8s dashboard").Default("false").BoolVar(&g.Dashboard)
+
+	k8sGKECluster := k8sGKE.Command("cluster", "cluster commands")
+	k8sGKECluster.Command("create", "create a new k8s cluster").Action(g.ClusterCreate)
+	k8sGKECluster.Command("delete", "delete a k8s cluster").Action(g.ClusterDelete)
+	k8sGKECluster.Command("list", "list k8s clusters").Action(g.ClusterList)
+	k8sGKECluster.Command("get", "get details for a k8s cluster").Action(g.ClusterGet)
+
+	k8sGKEDeployment := k8sGKE.Command("deployment", "deployment commands").Action(g.NewDeploymentClient)
+	k8sGKEDeployment.Flag("file", "deployment manifest file").Short('f').Required().ExistingFilesVar(&g.Deployments)
+	k8sGKEDeployment.Command("apply", "apply a k8s deployment manifest").Action(g.DeploymentApply)
+	k8sGKEDeployment.Command("delete", "delete a k8s deployment").Action(g.DeploymentDelete)
 
 	if _, err := app.Parse(os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
