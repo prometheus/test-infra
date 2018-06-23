5be65f5bce72de25fb52e8110c36a78fd05d2e2e
Harsh Agarwal <cs15btech11019@iith.ac.in>
2018-06-22 23:42:24 -0400

    verify if deployment, daemonset were created successfully. namespace deleted successfully
     Signed-off-by: Harsh Agarwal <cs15btech11019@iith.ac.in>

--- provider/gke/resourceCheck.go
+++ provider/gke/resourceCheck.go
@@ -2,22 +2,24 @@ package gke
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	apiCoreV1 "k8s.io/api/core/v1"
+	apiExtensionsV1beta1 "k8s.io/api/extensions/v1beta1"
 	apiMetaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
-func (c *GKE) waitForService(resource runtime.Object) error {
+func (c *GKE) waitForService(resource runtime.Object) {
 	req := resource.(*apiCoreV1.Service)
 	client := c.clientset.CoreV1().Services(req.Namespace)
 
-	for {
+	for i := 1; i <= maxTries; i++ {
 		res, err := client.Get(req.Name, apiMetaV1.GetOptions{})
 		if err != nil {
-			log.Fatalf("Checking resource status failed  %v", err)
+			log.Fatalf("Checking Service resource status failed  %v", err)
 		}
 		if res.Spec.Type == "LoadBalancer" {
 			// k8s API currently just supports LoadBalancerStatus
@@ -26,14 +28,71 @@ func (c *GKE) waitForService(resource runtime.Object) error {
 				for _, x := range res.Status.LoadBalancer.Ingress {
 					log.Printf("\t\thttp://%s:%d", x.IP, res.Spec.Ports[0].Port)
 				}
-				return nil
+				return
 			}
 			retry := time.Second * 10
-			log.Printf("Service %v external IP is being created. Waiting for it to be created.", req.Name)
+			log.Printf("Service %v external IP is being created. Retrying after 10 seconds.", req.Name)
 			time.Sleep(retry)
 		} else {
-			return nil
+			return
 		}
 	}
-	return nil
+	log.Fatalf("Service %v was not created after trying %d times", req.Name, maxTries)
+}
+
+func (c *GKE) waitForDeployment(resource runtime.Object) {
+	req := resource.(*apiExtensionsV1beta1.Deployment)
+	client := c.clientset.ExtensionsV1beta1().Deployments(req.Namespace)
+
+	for i := 1; i <= maxTries; i++ {
+		res, err := client.Get(req.Name, apiMetaV1.GetOptions{})
+		if err != nil {
+			log.Fatalf("Checking Deployment resource status failed  %v", err)
+		}
+		if res.Status.UnavailableReplicas == 0 {
+			return
+		}
+		retry := time.Second * 10
+		log.Printf("Deployment %v is being created. Retrying after 10 seconds.", req.Name)
+		time.Sleep(retry)
+	}
+	log.Fatalf("Deployment %v was not created after trying %d times", req.Name, maxTries)
+}
+
+func (c *GKE) waitForDaemonSet(resource runtime.Object) {
+	req := resource.(*apiExtensionsV1beta1.DaemonSet)
+	client := c.clientset.ExtensionsV1beta1().DaemonSets(req.Namespace)
+
+	for i := 1; i <= maxTries; i++ {
+		res, err := client.Get(req.Name, apiMetaV1.GetOptions{})
+		if err != nil {
+			log.Fatalf("Checking DaemonSet resource status failed  %v", err)
+		}
+		if res.Status.NumberUnavailable == 0 {
+			return
+		}
+		retry := time.Second * 10
+		log.Printf("DaemonSet %v is being created. Retrying after 10 seconds.", req.Name)
+		time.Sleep(retry)
+	}
+	log.Fatalf("DaemonSet %v was not created after trying %d times", req.Name, maxTries)
+}
+
+func (c *GKE) waitForNameSpaceDeletion(resource runtime.Object) {
+	req := resource.(*apiCoreV1.Namespace)
+	client := c.clientset.CoreV1().Namespaces()
+
+	for i := 1; i <= maxTries; i++ {
+		_, err := client.Get(req.Name, apiMetaV1.GetOptions{})
+		if err != nil {
+			if strings.Contains(err.Error(), "not found") {
+				return
+			}
+			log.Fatalf("Couldn't get namespace info:%v", err)
+		}
+		retry := time.Second * 10
+		log.Printf("All the components of namespace %v are being deleted. Retrying after 10 seconds.", req.Name)
+		time.Sleep(retry)
+	}
+	log.Fatalf("Namespace %v was not deleted after trying %d times", req.Name, maxTries)
 }
