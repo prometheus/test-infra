573cb45f4213614d98a078a094b15ed12ab987c2
Harsh Agarwal <cs15btech11019@iith.ac.in>
2018-07-05 19:29:26 -0400

    Add cluster create/delete function. Integrate along with with node-pool operations Signed-off-by: Harsh Agarwal <cs15btech11019@iith.ac.in>
    Signed-off-by: Harsh Agarwal <cs15btech11019@iith.ac.in>

--- cmd/prombench/main.go
+++ cmd/prombench/main.go
@@ -16,38 +16,53 @@ func main() {
 	app.HelpFlag.Short('h')
 
 	g := gke.New()
-	k8sGKE := app.Command("gke", `Google container engine provider - https://cloud.google.com/kubernetes-engine/.Requires`).
+	k8sGKE := app.Command("gke", `Google container engine provider - https://cloud.google.com/kubernetes-engine/`).
 		Action(g.NewGKEClient).
 		Action(g.ConfigParse)
-	k8sGKE.Flag("config", "yaml GKE config file used to scale_up or scale_down the prow cluster").
-		PlaceHolder("node-pool.yaml").
-		Short('c').
-		Default("../../config/node-pool.yaml").
-		ExistingFileVar(&g.ClusterConfigFile)
-	k8sGKE.Flag("auth", "json authentication file for the project - https://cloud.google.com/iam/docs/creating-managing-service-account-keys. If not set the tool will use the GOOGLE_APPLICATION_CREDENTIALS env variable (export GOOGLE_APPLICATION_CREDENTIALS=key.json)").
-		PlaceHolder("key.json").
+	k8sGKE.Flag("auth", "json authentication file for the project - https://cloud.google.com/iam/docs/creating-managing-service-account-keys. If not set the tool will use the GOOGLE_APPLICATION_CREDENTIALS env variable (export GOOGLE_APPLICATION_CREDENTIALS=service-account.json)").
+		Required().
+		PlaceHolder("service-account.json").
 		Short('a').
 		ExistingFileVar(&g.AuthFile)
-	k8sGKE.Flag("vars", "When provided it will substitute the token holders in the resources file. Follows the standard golang template formating - {{ .hashStable }}.").
+	k8sGKE.Flag("config", "GKE cluster-config yaml file").
+		PlaceHolder("cluster.yaml").
+		Short('c').
+		Default("config/cluster.yaml").
+		ExistingFileVar(&g.ConfigFile)
+	k8sGKE.Flag("pool", "GKE Nodepool-config yaml file").
+		PlaceHolder("node-pool.yaml").
+		Short('p').
+		Default("config/node-pool.yaml").
+		ExistingFileVar(&g.NodePoolConfigFile)
+	k8sGKE.Flag("file", "yaml file used to apply or delete k8s resources. If directory is given, all the yaml files from are read recursively from it.").
+		PlaceHolder("resources.yaml").
+		Short('f').
+		Default("manifests").
+		ExistingFilesOrDirsVar(&g.ResourceFiles)
+	k8sGKE.Flag("vars", "When provided it will substitute the token holders in the yaml file. Follows the standard golang template formating - {{ .hashStable }}.").
 		Short('v').
 		StringMapVar(&g.ResourceVars)
 
-	k8sGKECluster := k8sGKE.Command("cluster", "Scale up or down a k8s clusters by creating node-pools")
-	k8sGKECluster.Command("scaleUp", "gke cluster scaleUp -a key.json  -c ../../config/node-pool.yaml").
+	// cluster operations
+	k8sGKECluster := k8sGKE.Command("cluster", "Create or delete GKE k8s clusters")
+	k8sGKECluster.Command("create", "gke cluster create -a service-account.json  -c config/cluster.yaml").
+		Action(g.ClusterCreate)
+	k8sGKECluster.Command("delete", "gke cluster delete -a service-account.json  -c config/cluster.yaml").
+		Action(g.ClusterDelete)
+
+	// node-pool operations
+	k8sGKENodePool := k8sGKE.Command("nodepool", "Scale up or down a k8s clusters using node-pools").
+		Action(g.NodePoolConfigParse)
+	k8sGKENodePool.Command("create", "gke nodepool create -a service-account.json  -c config/cluster.yaml -p config/node-pool.yaml").
 		Action(g.NodePoolCreate)
-	k8sGKECluster.Command("scaleDown", "gke cluster scaleDown -a key.json  -c ../../config/node-pool.yaml").
+	k8sGKENodePool.Command("delete", "gke nodepool delete -a service-account.json  -c config/cluster.yaml -p config/node-pool.yaml").
 		Action(g.NodePoolDelete)
 
 	k8sGKEResource := k8sGKE.Command("resource", "Create,update and delete different k8s resources - deployments, services, config maps etc.").
 		Action(g.NewResourceClient)
-	k8sGKEResource.Flag("file", "yaml file used to apply or delete k8s resources. If directory is given, all the yaml files from are read recursively from it. It uses the standard k8s formatting. It also supports the default golang templates.").
-		PlaceHolder("resources.yaml").
-		Short('f').
-		Default("../../config/resources.yaml").
-		ExistingFilesOrDirsVar(&g.ResourceFiles)
-	k8sGKEResource.Command("apply", "gke resource apply -a ../../config/key.json -c ../../config/node-pool.yaml -f ../../config/resources.yaml --vars hashStable:COMMIT1 --vars hashTesting:COMMIT2").
+	k8sGKEResource.Command("apply", "gke resource apply -a service-account.json -c config/cluster.yaml -f manifests -v hashStable:COMMIT1 -v hashTesting:COMMIT2").
 		Action(g.ResourceApply)
-	k8sGKEResource.Command("delete", "gke resource delete -a ../../config/key.json -c ../../config/node-pool.yaml -f ../../config/resources.yaml --vars hashStable:COMMIT1 --vars hashTesting:COMMIT2").
+	k8sGKEResource.Command("delete", "gke resource delete -a service-account.json -c config/cluster.yaml -f manifests -v hashStable:COMMIT1 -v hashTesting:COMMIT2").
 		Action(g.ResourceDelete)
 
 	if _, err := app.Parse(os.Args[1:]); err != nil {
