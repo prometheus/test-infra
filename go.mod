8c8af4381f9f6f67ea76a3c33f1091c58c35f2d5
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-06-23 02:58:27 +0530

    added amghrcvr
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	cloud.google.com/go v0.39.0
-	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/google/go-github/v26 v26.0.6
 	github.com/google/gofuzz v1.0.0 // indirect
 	github.com/googleapis/gnostic v0.2.0 // indirect
 	github.com/imdario/mergo v0.3.7 // indirect
@@ -12,9 +12,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/prometheus/alertmanager v0.17.0
 	github.com/prometheus/client_golang v0.9.3
 	github.com/spf13/pflag v1.0.3 // indirect
 	google.golang.org/api v0.5.0
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
 	google.golang.org/genproto v0.0.0-20190516172635-bb713bdc0e52
 	google.golang.org/grpc v1.19.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
---
+++ tools/amghrcvr/template.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"html/template"
+	"log"
+	"strconv"
+
+	"github.com/prometheus/alertmanager/notify"
+)
+
+const alertMD = `
+Alertmanager URL: {{.Data.ExternalURL}}
+{{range .Data.Alerts}}
+  * {{.Status}} {{.GeneratorURL}}
+  {{if .Labels}}
+    Labels:
+  {{- end}}
+  {{range $key, $value := .Labels}}
+    - {{$key}} = {{$value -}}
+  {{end}}
+  {{if .Annotations}}
+    Annotations:
+  {{- end}}
+  {{range $key, $value := .Annotations}}
+    - {{$key}} = {{$value -}}
+  {{end}}
+{{end}}
+
+TODO: add graph url from annotations.
+`
+
+var alertTemplate = template.Must(template.New("alert").Parse(alertMD))
+
+// id returns the alert id
+func id(msg *notify.WebhookMessage) string {
+	return fmt.Sprintf("0x%x", msg.GroupKey)
+}
+
+// formatTitle constructs an issue title from a webhook message.
+func formatTitle(msg *notify.WebhookMessage) string {
+	return fmt.Sprintf("%s", msg.Data.GroupLabels["alertname"])
+}
+
+// formatIssueBody constructs an issue body from a webhook message.
+func formatIssueBody(msg *notify.WebhookMessage) (string, error) {
+	var buf bytes.Buffer
+	err := alertTemplate.Execute(&buf, msg)
+	if err != nil {
+		log.Printf("Error executing template: %s", err)
+		return "", err
+	}
+	s := buf.String()
+	// do we need the alert id in the comment? i dont think so
+	return fmt.Sprintf("<!-- ID: %s -->\n%s", id(msg), s), nil
+}
+
+// getTargetRepo returns the "repo" label if exists else returns defaultRepo
+func getTargetRepo(msg *notify.WebhookMessage) string {
+	if repo, ok := msg.CommonLabels["repo"]; ok {
+		return repo
+	}
+	return defaultRepo
+}
+
+// getTargetOwner returns the "owner" label if exists else returns defaultOrg
+func getTargetOwner(msg *notify.WebhookMessage) string {
+	if owner, ok := msg.CommonLabels["owner"]; ok {
+		return owner
+	}
+	return defaultOwner
+}
+
+// getTargetPR returns the "prno" label
+func getTargetPR(msg *notify.WebhookMessage) (int, error) {
+	if prNo, ok := msg.CommonLabels["prno"]; ok {
+		i, err := strconv.Atoi(prNo)
+		if err != nil {
+			return 0, err
+		}
+		return i, nil
+	}
+	return 0, errors.New("prno label not found")
+}
