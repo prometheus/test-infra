ea8ffb52f812da542916890374d42ae7f397c2be
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-06-26 12:59:37 +0530

    removed global vars from amgh
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- tools/amgh/main.go
+++ tools/amgh/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,94 +16,113 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var (
-	authfile       string
-	defaultOwner   string
-	defaultRepo    string
-	ghClient       *github.Client
-	issueSvc       *github.IssuesService
-	ctx, cancelCtx = context.WithCancel(context.Background())
-)
+type ghWebhookRecieverConfig struct {
+	authfile     string
+	defaultOwner string
+	defaultRepo  string
+	portNo       string
+}
 
-func processAlert(msg *notify.WebhookMessage) error {
+type ghWebhookReciever struct {
+	ghClient *github.Client
+	cfg      ghWebhookRecieverConfig
+}
 
-	// add comment if firing
-	if msg.Data.Status == "firing" {
-		msgBody, err := formatIssueBody(msg)
-		if err != nil {
-			return err
-		}
+type ghWebhookHandler struct {
+	client *ghWebhookReciever
+}
 
-		prNo, err := getTargetPR(msg)
-		if err != nil {
-			return err
-		}
-		issueComment := github.IssueComment{Body: &msgBody}
-		_, _, err = issueSvc.CreateComment(ctx, getTargetOwner(msg), getTargetRepo(msg), prNo, &issueComment)
-		if err != nil {
-			return err
-		}
+func (hl ghWebhookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Printf("unsupported request method: %v: %v", r.Method, r.RemoteAddr)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	msg := &notify.WebhookMessage{}
+	ctx := r.Context()
+
+	// Decode the webhook request.
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		log.Println("failed to decode webhook data")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Handle the webhook message.
+	log.Printf("handling alert: %v", alertID(msg))
+	if err := hl.client.processAlert(ctx, msg); err != nil {
+		log.Printf("failed to handle alert: %v: %v", alertID(msg), err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("completed alert: %v", alertID(msg))
+	w.WriteHeader(http.StatusOK)
+}
+
+func newGhWebhookReciever(cfg ghWebhookRecieverConfig) (*ghWebhookReciever, error) {
+	oauth2token, err := ioutil.ReadFile(cfg.authfile)
+	if err != nil {
+		return nil, err
+	}
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: string(oauth2token)},
+	)
+	ctx := context.Background()
+	tc := oauth2.NewClient(ctx, ts)
+	return &ghWebhookReciever{
+		ghClient: github.NewClient(tc),
+		cfg:      cfg,
+	}, nil
+}
+
+// processAlert formats and posts the comment to github and returns nil if successful.
+func (g ghWebhookReciever) processAlert(ctx context.Context, msg *notify.WebhookMessage) error {
+
+	msgBody, err := formatIssueBody(msg)
+	if err != nil {
+		return err
+	}
+	issueComment := github.IssueComment{Body: &msgBody}
+
+	prNum, err := getTargetPR(msg)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = g.ghClient.Issues.CreateComment(ctx,
+		g.getTargetOwner(msg), g.getTargetRepo(msg), prNum, &issueComment)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func handleWebhook(rw http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		log.Printf("Client used unsupported method: %s: %s", r.Method, r.RemoteAddr)
-		rw.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	msg := &notify.WebhookMessage{}
-
-	// decode the webhook request
-	err := json.NewDecoder(r.Body).Decode(&msg)
-	if err != nil {
-		log.Println("Failed to decode json")
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Handle the webhook message.
-	log.Printf("Handling alert: %s", id(msg))
-	if err := processAlert(msg); err != nil {
-		log.Printf("Failed to handle alert: %s: %s", id(msg), err)
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	log.Printf("Completed alert: %s", id(msg))
-	rw.WriteHeader(http.StatusOK)
-
-	return
+func serveWebhook(client *ghWebhookReciever) {
+	hl := ghWebhookHandler{client}
+	http.Handle("/hook", hl)
+	log.Printf("finished setting up gh client. starting amgh with %v/%v as defaults",
+		client.cfg.defaultOwner, client.cfg.defaultRepo)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", client.cfg.portNo), nil))
 }
 
 func main() {
-	app := kingpin.New(filepath.Base(os.Args[0]), "amgh - alertmanager github reciever")
-	app.Flag("authfile", "path to github oauth token file").Default("/etc/github").StringVar(&authfile)
-	app.Flag("org", "default org/owner").Required().StringVar(&defaultOwner)
-	app.Flag("repo", "default repo").Required().StringVar(&defaultRepo)
+	cfg := ghWebhookRecieverConfig{}
+
+	app := kingpin.New(filepath.Base(os.Args[0]), "alertmanager github webhook reciever")
+	app.Flag("authfile", "path to github oauth token file").Default("/etc/github").StringVar(&cfg.authfile)
+	app.Flag("org", "default org/owner").Required().StringVar(&cfg.defaultOwner)
+	app.Flag("repo", "default repo").Required().StringVar(&cfg.defaultRepo)
+	app.Flag("port", "port number to run the server in").Default("8080").StringVar(&cfg.portNo)
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	// setup ghClient
-	oauth2token, err := ioutil.ReadFile(authfile)
+	client, err := newGhWebhookReciever(cfg)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("failed to create GitHub Webhook Reciever client: %v", err)
 	}
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: string(oauth2token)},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	ghClient = github.NewClient(tc)
-	issueSvc = ghClient.Issues
-	log.Println("GitHub client successfully setup.")
 
-	// start webhook server
-	log.Printf("Started amgh %v/%v as defaults.", defaultOwner, defaultRepo)
-	http.HandleFunc("/hook", handleWebhook)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	<-ctx.Done()
+	serveWebhook(client)
 }
