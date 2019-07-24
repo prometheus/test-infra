6358c1a815cfc2fc382a6260c4600a3462072812
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-07-24 10:30:49 +0530

    used local config-bootstrapper hack to use docker image
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- hack/config-bootstrapper.sh
+++ hack/config-bootstrapper.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+CB_DOCKER_IMAGE="gcr.io/k8s-prow/config-bootstrapper:v20190608-493ef838c"
 dir=$(realpath "$(dirname "${BASH_SOURCE}")/..")
-config-bootstrapper \
+
+docker run --rm \
+    -v $dir:/prombench \
+    -v $HOME/.kube:/kube \
+    --network host \
+    $CB_DOCKER_IMAGE \
     --dry-run=false \
-    --source-path "${dir}" \
-    --config-path "${dir}/config/prow/config.yaml" \
-    --plugin-config "${dir}/config/prow/plugins.yaml"
+    --source-path /prombench \
+    --config-path /prombench/config/prow/config.yaml \
+    --plugin-config /prombench/config/prow/plugins.yaml \
+    --kubeconfig=/kube/$(basename $KUBECONFIG)
