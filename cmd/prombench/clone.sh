0b40822f21b2f964c4e91d697373c37e90ae125c
nikita-mk <nikkikokitkar@gmail.com>
2019-08-02 09:43:32 +0530

    add cd into prombench directory
    Signed-off-by: nikita-mk <nikkikokitkar@gmail.com>

--- cmd/prombench/clone.sh
+++ cmd/prombench/clone.sh
@@ -6,5 +6,6 @@ git clone $PROMBENCH_REPO $PROMBENCH_DIR
 # copy the prombench binary to the cloned directory
 cp /usr/bin/prombench $PROMBENCH_DIR/
 
+cd $PROMBENCH_DIR
 # execute arguments passed to the image
-$@
+$*
