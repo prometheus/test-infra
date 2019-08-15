74771ad154858c4eb525aa1344267c67e50f7a62
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-08-15 21:37:56 +0530

    [WIP] Add GITHUB_ORG and GITHUB_REPO to prometheus-builder
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- temp/prometheus-builder/build.sh
+++ temp/prometheus-builder/build.sh
@@ -2,20 +2,20 @@
 
 DIR="/go/src/github.com/prometheus/prometheus"
 
-if [[ -z $PR_NUMBER || -z $VOLUME_DIR ]]; then
+if [[ -z $PR_NUMBER || -z $VOLUME_DIR || -z $GITHUB_ORG || -z $GITHUB_REPO ]]; then
     echo "ERROR:: environment variables not set correctly"
     exit 1;
 fi
 
-echo ">> Cloning repository prometheus/prometheus"
-if ! git clone https://github.com/prometheus/prometheus.git $DIR; then
-    echo "ERROR:: Cloning of repo prometheus/prometheus failed"
+echo ">> Cloning repository $GITHUB_ORG/$GITHUB_REPO"
+if ! git clone https://github.com/$GITHUB_ORG/$GITHUB_REPO.git $DIR; then
+    echo "ERROR:: Cloning of repo $GITHUB_ORG/$GITHUB_REPO failed"
     exit 1;
 fi
 
 cd $DIR || exit 1
 
-echo ">> Fetching Pull Request prometheus/prometheus/pull/$PR_NUMBER"
+echo ">> Fetching Pull Request $GITHUB_ORG/$GITHUB_REPO/pull/$PR_NUMBER"
 if ! git fetch origin pull/$PR_NUMBER/head:pr-branch; then
     echo "ERROR:: Fetching of PR $PR_NUMBER failed"
     exit 1;
