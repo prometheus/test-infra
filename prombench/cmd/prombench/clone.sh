faf45efb6768b62b01cb5d55b33c223f185b3d56
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-08-22 13:56:22 +0530

    changed PROMBENCH_REPO to TEST_INFRA_REPO
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- prombench/cmd/prombench/clone.sh
+++ prombench/cmd/prombench/clone.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# clone the prombench repo for latest Makefile and manifest files
-git clone $PROMBENCH_REPO $PROMBENCH_DIR
+# clone the test-infra repo for latest Makefile and manifest files
+git clone $TEST_INFRA_REPO $TEST_INFRA_DIR
 
 # copy the prombench binary to the cloned directory
 cp /usr/bin/prombench $PROMBENCH_DIR/
 
-cd $PROMBENCH_DIR
 # execute arguments passed to the image
 # eval is needed so that bash keywords are not run as commands
 eval "$@"
