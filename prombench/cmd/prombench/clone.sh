3961622563d865d61cbb9f16da4e5895db9914ea
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-08-26 11:09:21 +0530

    updated workdir and statefulset
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- prombench/cmd/prombench/clone.sh
+++ prombench/cmd/prombench/clone.sh
@@ -3,10 +3,11 @@
 # clone the test-infra repo for latest Makefile and manifest files
 git clone $TEST_INFRA_REPO $TEST_INFRA_DIR
 
+# copy prombench files to $PROMBENCH_DIR
+cp -r $TEST_INFRA_DIR/prombench/* $PROMBENCH_DIR/
 # copy the prombench binary to the cloned directory
 cp /usr/bin/prombench $PROMBENCH_DIR/
 
-cd $PROMBENCH_DIR
 # execute arguments passed to the image
 # eval is needed so that bash keywords are not run as commands
 eval "$@"
