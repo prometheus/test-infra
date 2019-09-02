aab1110c72b9656e0ce562b0d18c78ec81497349
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-09-02 18:41:15 +0530

    one script to handle signals to supervisord
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- prombench/cmd/prombench/supervisor-scripts/signal-supervisor.sh
+++ prombench/cmd/prombench/supervisor-scripts/signal-supervisor.sh
@@ -6,12 +6,15 @@ while true; do
 
   read line
 
-  # Get PAYLOAD_LENGTH
+  # Get EVENT_NAME and PAYLOAD_LENGTH
   for keypair in $line; do
     key="$(echo "$keypair" | cut -d ":" -f1)"
     value="$(echo "$keypair" | cut -d ":" -f2)"
     if [ "$key" == "len" ]; then
       PAYLOAD_LENGTH=$(($value))
+    fi
+    if [ "$key" == "eventname" ]; then
+      EVENT_NAME=$value
     fi
   done
 
@@ -25,14 +28,24 @@ while true; do
     if [ "$key" == "processname" ]; then
       PROCESS_NAME=$value
     fi
+    if [ "$key" == "expected" ]; then
+      # 0 if the exit code was unexpected.
+      # 1 if the exit code was expected.
+      EXPECTED_EXIT_CODE=$value
+    fi
   done
 
   printf "RESULT 2\nOK";
 
-  # Send SIGQUIT if any of the following is true:
+  # SIGKILL if:
+  # - EVENT_NAME is PROCESS_STATE_FATAL
+  # - EXPECTED_EXIT_CODE is unexpected
+  # SIGQUIT if:
   # - Anything other than init process exited
   # - INIT_EXIT env var is set
-  if [[ "$PROCESS_NAME" != "init" || -n "$INIT_EXIT" ]]; then
+  if [[ "$EVENT_NAME" == "PROCESS_STATE_FATAL" || "$EXPECTED_EXIT_CODE" == "0" ]]; then
+    killall -SIGKILL supervisord
+  elif [[ "$PROCESS_NAME" != "init" || -n "$INIT_EXIT" ]]; then
     killall -SIGQUIT supervisord
   fi
 
--- prombench/cmd/prombench/supervisor-scripts/sigterm-supervisor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while true; do
-  printf "READY\n";
-  read line
-  echo "Processing Event: $line" >&2;
-  printf "RESULT 2\nOK";
-  killall -SIGTERM supervisord
-done
