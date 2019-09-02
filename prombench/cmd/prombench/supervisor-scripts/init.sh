ec60fe81c29c8e62e5f73eedb9f8e2d480525511
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-09-02 22:15:40 +0530

    SHELL_COMMAND to init script
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- prombench/cmd/prombench/supervisor-scripts/init.sh
+++ prombench/cmd/prombench/supervisor-scripts/init.sh
@@ -5,4 +5,6 @@ git clone $TEST_INFRA_REPO $TEST_INFRA_DIR
 
 # copy the prombench binary to the cloned directory
 cp /usr/bin/prombench $PROMBENCH_DIR/
-cp -r $TEST_INFRA_DIR/prombench/* $PROMBENCH_DIR/ 
+cp -r $TEST_INFRA_DIR/prombench/* $PROMBENCH_DIR/
+
+$SHELL_COMMAND
