b8a10777d5a6808af04976bf3255e04198f35bad
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-11-17 13:47:07 +0530

    Added circle-ci artifacts to prometheus-builder
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- prombench/temp/prometheus-builder/build.sh
+++ prombench/temp/prometheus-builder/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-DIR="/go/src/github.com/prometheus/prometheus"
+CIRCLECI_BASE_API_URL="https://circleci.com/api/v1.1/project/github"
+FILTER="successful"
+CIRCLE_JOB="build"
+OS="linux"
+ARCH="amd64"
 
-if [[ -z $PR_NUMBER || -z $VOLUME_DIR || -z $GITHUB_ORG || -z $GITHUB_REPO ]]; then
+DIR="/tmp/prometheus"
+rm -rf $DIR
+
+if [[ -z $PR_NUMBER || -z $VOLUME_DIR || -z $GITHUB_ORG || -z $GITHUB_REPO || -z $LAST_COMMIT_SHA ]]; then
     echo "ERROR:: environment variables not set correctly"
     exit 1;
 fi
 
+# Clone git repo
 echo ">> Cloning repository $GITHUB_ORG/$GITHUB_REPO"
 if ! git clone https://github.com/$GITHUB_ORG/$GITHUB_REPO.git $DIR; then
     echo "ERROR:: Cloning of repo $GITHUB_ORG/$GITHUB_REPO failed"
@@ -15,20 +23,40 @@ fi
 
 cd $DIR || exit 1
 
+# Checkout PR
 echo ">> Fetching Pull Request $GITHUB_ORG/$GITHUB_REPO/pull/$PR_NUMBER"
 if ! git fetch origin pull/$PR_NUMBER/head:pr-branch; then
     echo "ERROR:: Fetching of PR $PR_NUMBER failed"
     exit 1;
 fi
-
 git checkout pr-branch
 
-echo ">> Creating prometheus binaries"
-if ! make build PROMU_BINARIES="prometheus"; then
-    echo "ERROR:: Building of binaries failed"
+# jq command to get the build.
+JQ_ARG='[.[] | select(.build_parameters.CIRCLE_JOB==$JOB)][0] | {build_id: .build_num, v_rev: .vcs_revision}'
+
+# Builds are returned in the order that they were created.
+URL="$CIRCLECI_BASE_API_URL/$GITHUB_ORG/$GITHUB_REPO/tree/pull/$PR_NUMBER?filter=$FILTER"
+BUILD=$(curl -s "$URL" | jq --arg JOB $CIRCLE_JOB "$JQ_ARG")
+BUILD_ID=$(echo $BUILD | jq -r '.build_id' )
+BUILD_REV=$(echo $BUILD | jq -r '.v_rev' )
+
+# Check if the build is of the latest commit.
+if [ $LAST_COMMIT_SHA != $BUILD_REV ]; then
+    echo "Last commit hash and the commit hash of the circleci build don't match. exiting"
     exit 1;
 fi
 
+# Get the artifact url.
+JQ_ARG='.[] | select(.path==$PATH) | .url'
+URL="$CIRCLECI_BASE_API_URL/$GITHUB_ORG/$GITHUB_REPO/$BUILD_ID/artifacts"
+PROMETHEUS_BIN_URL=$(curl -s "$URL" | jq -r --arg PATH "build/$OS-$ARCH/prometheus" "$JQ_ARG")
+
+# Download the artifact.
+echo "Downloading artifact: $PROMETHEUS_BIN_URL"
+curl -O "$PROMETHEUS_BIN_URL"
+chmod u+x prometheus
+
+# Copy files to mounted volume.
 echo ">> Copy files to volume"
 cp prometheus               $VOLUME_DIR/prometheus
 cp -r console_libraries/    $VOLUME_DIR
