be550a51c589b830cf146e256c4bfdf6069d49c7
Bartlomiej Plotka <bwplotka@gmail.com>
2020-02-20 23:14:20 +0000

    funcbench: Allow running locally on existing repo; Simple compare between versions.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

---
+++ funcbench/bench.go
@@ -0,0 +1,142 @@
+// Copyright 2020 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"golang.org/x/tools/benchmark/parse"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+)
+
+// TODO: Add unit test.
+type Benchmarker struct {
+	logger Logger
+
+	benchFunc      string
+	benchTime      time.Duration
+	benchTimeout   time.Duration
+	resultCacheDir string
+
+	c    *commander
+	repo *git.Repository
+}
+
+func newBenchmarker(logger Logger, env Environment, c *commander, benchTime time.Duration, benchTimeout time.Duration, resultCacheDir string) *Benchmarker {
+	return &Benchmarker{
+		logger:         logger,
+		benchFunc:      env.BenchFunc(),
+		benchTime:      benchTime,
+		benchTimeout:   benchTimeout,
+		c:              c,
+		repo:           env.Repo(),
+		resultCacheDir: resultCacheDir,
+	}
+}
+
+func (b *Benchmarker) benchOutFileName(commit plumbing.Hash) (string, error) {
+	// Sanitize bench func.
+	bb := bytes.Buffer{}
+	e := base64.NewEncoder(base64.StdEncoding, &bb)
+	if _, err := e.Write([]byte(b.benchFunc)); err != nil {
+		return "", err
+	}
+	if err := e.Close(); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s-%s.out", bb.String(), commit.String()), nil
+}
+
+func (b *Benchmarker) execBenchmark(pkgRoot string, commit plumbing.Hash) (out string, err error) {
+	fileName, err := b.benchOutFileName(commit)
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := ioutil.ReadFile(filepath.Join(b.resultCacheDir, fileName)); err == nil {
+		fmt.Println("Found previous results for ", fileName, b.benchFunc, "Reusing.")
+		return filepath.Join(b.resultCacheDir, fileName), nil
+	}
+
+	// TODO(bwplotka): Allow memprofiles.
+	extraArgs := []string{"-benchtime", b.benchTime.String()}
+	extraArgs = append(extraArgs, "-timeout", b.benchTimeout.String())
+	benchCmd := []string{"bash", "-c", strings.Join(append(append([]string{"cd", pkgRoot, "&&", "go", "test", "-run", "^$", "-bench", fmt.Sprintf("^%s$", b.benchFunc), "-benchmem"}, extraArgs...), "./..."), " ")}
+
+	b.logger.Println("Executing benchmark cmd:", benchCmd)
+	out, err = b.c.exec(benchCmd...)
+	if err != nil {
+		return "", errors.Wrap(err, "benchmark ended with an error.")
+	}
+
+	fn := fileName
+	if b.resultCacheDir != "" {
+		fn = filepath.Join(b.resultCacheDir, fileName)
+	}
+	if err := ioutil.WriteFile(fn, []byte(out), os.ModePerm); err != nil {
+		return "", err
+	}
+	return fn, nil
+}
+
+func parseFile(path string) (parse.Set, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	bb, err := parse.ParseSet(f)
+	if err != nil {
+		return nil, err
+	}
+	return bb, nil
+}
+
+func (b *Benchmarker) compareBenchmarks(beforeFile, afterFile string) ([]BenchCmp, error) {
+	before, err := parseFile(beforeFile)
+	if err != nil {
+		return nil, errors.Wrapf(err, "open %s", beforeFile)
+	}
+
+	after, err := parseFile(afterFile)
+	if err != nil {
+		return nil, errors.Wrapf(err, "open %s", afterFile)
+	}
+
+	// Use benchcmp library directly.
+	// TODO(bwplotka): benchstat is new thing - we might add choice for funcbench to choose from? (:
+	cmps, warnings := Correlate(before, after)
+	for _, warn := range warnings {
+		b.logger.Println(warn)
+	}
+	if len(cmps) == 0 {
+		return nil, errors.New("no repeated benchmarks")
+	}
+
+	return cmps, nil
+}
+
+func (b *Benchmarker) compareSubBenchmarks(string) ([]BenchCmp, error) {
+	// TODO(bwplotka): Implement.
+	return nil, errors.New("not implemented")
+}
---
+++ funcbench/env.go
@@ -0,0 +1,229 @@
+// Copyright 2020 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+)
+
+type Environment interface {
+	BenchFunc() string
+	CompareTarget() string
+
+	PostErr(err string) error
+	PostResults(cmps []BenchCmp) error
+
+	Repo() *git.Repository
+}
+
+type environment struct {
+	logger Logger
+
+	benchFunc     string
+	compareTarget string
+
+	home string
+}
+
+func (e environment) BenchFunc() string     { return e.benchFunc }
+func (e environment) CompareTarget() string { return e.compareTarget }
+
+type Local struct {
+	environment
+
+	repo *git.Repository
+}
+
+func newLocalEnv(e environment) (Environment, error) {
+	r, err := git.PlainOpenWithOptions(".", &git.PlainOpenOptions{DetectDotGit: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Local{environment: e, repo: r}, nil
+}
+
+func (l *Local) PostErr(string) error { return nil } // Noop. We will see error anyway.
+
+// formatNs formats ns measurements to expose a useful amount of
+// precision. It mirrors the ns precision logic of testing.B.
+func formatNs(ns float64) string {
+	prec := 0
+	switch {
+	case ns < 10:
+		prec = 2
+	case ns < 100:
+		prec = 1
+	}
+	return strconv.FormatFloat(ns, 'f', prec, 64)
+}
+
+func (l *Local) PostResults(cmps []BenchCmp) error {
+	fmt.Println("Results:")
+	Render(os.Stdout, cmps, false, false, l.compareTarget)
+	return nil
+}
+
+func (l *Local) Repo() *git.Repository { return l.repo }
+
+// TODO: Add unit test(!).
+type GitHub struct {
+	environment
+
+	repo    *git.Repository
+	client  *gitHubClient
+	logLink string
+}
+
+func newGitHubEnv(ctx context.Context, e environment, owner, repo string, prNumber int) (Environment, error) {
+	if err := os.Chdir(os.Getenv("GITHUB_WORKSPACE")); err != nil {
+		return nil, errors.Wrap(err, "changing to GITHUB_WORKSPACE dir")
+	}
+
+	ghClient := newGitHubClient(owner, repo, prNumber)
+	r, err := git.PlainCloneContext(ctx, ghClient.repo, false, &git.CloneOptions{
+		URL:      fmt.Sprintf("https://github.com/%s/%s.git", ghClient.owner, ghClient.repo),
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		// If repo already exists, git.ErrRepositoryAlreadyExists will be returned.
+		return nil, errors.Wrap(err, "git clone")
+	}
+
+	if err := os.Chdir(filepath.Join(os.Getenv("GITHUB_WORKSPACE"), ghClient.repo)); err != nil {
+		return nil, errors.Wrapf(err, "changing to GITHUB_WORKSPACE/%s dir", ghClient.repo)
+	}
+
+	g := &GitHub{
+		environment: e,
+		repo:        r,
+		client:      ghClient,
+		logLink:     fmt.Sprintf("Full logs at: https://github.com/%s/%s/commit/%s/checks", ghClient.owner, ghClient.repo, ghClient.latestCommitHash),
+	}
+
+	if err := os.Setenv("GO111MODULE", "on"); err != nil {
+		return nil, err
+	}
+	if err := os.Setenv("CGO_ENABLED", "0"); err != nil {
+		return nil, err
+	}
+
+	wt, err := g.repo.Worktree()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := r.FetchContext(ctx, &git.FetchOptions{}); err != nil && err != git.NoErrAlreadyUpToDate {
+		if pErr := g.PostErr("Switch (fetch) to a pull request branch failed"); pErr != nil {
+			return nil, errors.Wrapf(err, "posting a comment for `checkout` command execution error; postComment err:%v", pErr)
+		}
+		return nil, err
+	}
+
+	if err = wt.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(fmt.Sprintf("pull/%d/head:pullrequest", ghClient.prNumber)),
+	}); err != nil {
+		if pErr := g.PostErr("Switch to a pull request branch failed"); pErr != nil {
+			return nil, errors.Wrapf(err, "posting a comment for `checkout` command execution error; postComment err:%v", pErr)
+		}
+		return nil, err
+	}
+	return g, nil
+}
+
+func (g *GitHub) Repo() *git.Repository { return g.repo }
+
+type gitHubClient struct {
+	owner            string
+	repo             string
+	latestCommitHash string
+	prNumber         int
+	client           *github.Client
+}
+
+func newGitHubClient(owner, repo string, prNumber int) *gitHubClient {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})
+	tc := oauth2.NewClient(context.Background(), ts)
+	c := gitHubClient{
+		client:           github.NewClient(tc),
+		owner:            owner,
+		repo:             repo,
+		prNumber:         prNumber,
+		latestCommitHash: os.Getenv("GITHUB_SHA"),
+	}
+	return &c
+}
+
+func (c *gitHubClient) postComment(comment string) error {
+	issueComment := &github.IssueComment{Body: github.String(comment)}
+	_, _, err := c.client.Issues.CreateComment(context.Background(), c.owner, c.repo, c.prNumber, issueComment)
+	return err
+}
+
+func (g *GitHub) PostErr(err string) error {
+	if err := g.client.postComment(fmt.Sprintf("%v. Logs: %v", err, g.logLink)); err != nil {
+		return errors.Wrap(err, "posting err")
+	}
+	return nil
+}
+
+func (g *GitHub) PostResults(cmps []BenchCmp) error {
+	b := bytes.Buffer{}
+	Render(&b, cmps, false, false, g.compareTarget)
+	return g.client.postComment(formatCommentToMD(b.String()))
+}
+
+func formatCommentToMD(rawTable string) string {
+	tableContent := strings.Split(rawTable, "\n")
+	for i := 0; i <= len(tableContent)-1; i++ {
+		e := tableContent[i]
+		switch {
+		case e == "":
+
+		case strings.Contains(e, "old ns/op"):
+			e = "| Benchmark | Old ns/op | New ns/op | Delta |"
+			tableContent = append(tableContent[:i+1], append([]string{"|-|-|-|-|"}, tableContent[i+1:]...)...)
+
+		case strings.Contains(e, "old MB/s"):
+			e = "| Benchmark | Old MB/s | New MB/s | Speedup |"
+			tableContent = append(tableContent[:i+1], append([]string{"|-|-|-|-|"}, tableContent[i+1:]...)...)
+
+		case strings.Contains(e, "old allocs"):
+			e = "| Benchmark | Old allocs | New allocs | Delta |"
+			tableContent = append(tableContent[:i+1], append([]string{"|-|-|-|-|"}, tableContent[i+1:]...)...)
+
+		case strings.Contains(e, "old bytes"):
+			e = "| Benchmark | Old bytes | New bytes | Delta |"
+			tableContent = append(tableContent[:i+1], append([]string{"|-|-|-|-|"}, tableContent[i+1:]...)...)
+
+		default:
+			// Replace spaces with "|".
+			e = strings.Join(strings.Fields(e), "|")
+		}
+		tableContent[i] = e
+	}
+	return strings.Join(tableContent, "\n")
+
+}
