e483aeb92fea5476e6b5141db4f06993968aa812
Drumil Patel <drumilpatel720@gmail.com>
2020-05-25 17:50:20 +0530

    Add support for deploymentRangeVars
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- pkg/provider/eks/eks.go
+++ pkg/provider/eks/eks.go
@@ -603,3 +603,138 @@ func (c *EKS) GetDeploymentVars(*kingpin.ParseContext) error {
 
 	return nil
 }
+
+func (c *EKS) eksDeploymentVars() map[string]string {
+	deployVars := c.DeploymentVars
+
+	for key, rangeVars := range c.DeploymentRangeVars {
+		deployVars[key] = rangeVars
+	}
+
+	deployVars["SEPERATOR"] = c.Separator
+
+	return deployVars
+}
+
+// EKSDeploymentParse parses the cluster/nodegroups deployment file and saves the result as bytes grouped by the filename.
+// Any variables passed to the cli will be replaced in the resource files following the golang text template format.
+func (c *EKS) EKSDeploymentParse(*kingpin.ParseContext) error {
+
+	deploymentResource, err := provider.DeploymentsParse(c.DeploymentFiles, c.eksDeploymentVars())
+	if err != nil {
+		log.Fatalf("Couldn't parse deployment files: %v", err)
+	}
+
+	c.eksResources = deploymentResource
+	return nil
+}
+
+// K8SDeploymentsParse parses the k8s objects deployment files and saves the result as k8s objects grouped by the filename.
+// Any variables passed to the cli will be replaced in the resources files following the golang text template format.
+func (c *EKS) K8SDeploymentsParse(*kingpin.ParseContext) error {
+	deploymentResource, err := provider.DeploymentsParse(c.DeploymentFiles, c.DeploymentVars)
+	if err != nil {
+		log.Fatalf("Couldn't parse deployment files: %v", err)
+	}
+
+	for _, deployment := range deploymentResource {
+
+		decode := scheme.Codecs.UniversalDeserializer().Decode
+		k8sObjects := make([]runtime.Object, 0)
+
+		for _, text := range strings.Split(string(deployment.Content), provider.Separator) {
+			text = strings.TrimSpace(text)
+			if len(text) == 0 {
+				continue
+			}
+
+			resource, _, err := decode([]byte(text), nil, nil)
+			if err != nil {
+				return errors.Wrapf(err, "decoding the resource file:%v, section:%v...", deployment.FileName, text[:100])
+			}
+			if resource == nil {
+				continue
+			}
+			k8sObjects = append(k8sObjects, resource)
+		}
+		if len(k8sObjects) > 0 {
+			c.k8sResources = append(c.k8sResources, k8sProvider.Resource{FileName: deployment.FileName, Objects: k8sObjects})
+		}
+	}
+	return nil
+}
+
+// ClusterCreate create a new cluster or applies changes to an existing cluster.
+func (c *EKS) ClusterCreate(*kingpin.ParseContext) error {
+	req := eks.CreateClusterInput{}
+	for _, deployment := range c.eksResources {
+
+		if err := yamlGo.UnmarshalStrict(deployment.Content, req); err != nil {
+			log.Fatalf("Error parsing the cluster deployment file %s:%v", deployment.FileName, err)
+		}
+
+		log.Printf("Cluster create request: name:'%v'", req.Name)
+		_, err := c.clientEKS.CreateCluster(&req)
+		if err != nil {
+			log.Fatalf("Couldn't create cluster '%v', file:%v ,err: %v", req.Name, deployment.FileName, err)
+		}
+
+		// err = provider.RetryUntilTrue(
+		// 	fmt.Sprintf("creating cluster:%v", req.Name),
+		// 	provider.GlobalRetryCount,
+		// 	func() (bool, error) { return c.clusterRunning(req.Zone, req.ProjectId, req.Cluster.Name) })
+
+		if err != nil {
+			log.Fatalf("creating cluster err:%v", err)
+		}
+	}
+	return nil
+}
+
+// ClusterDelete deletes a eks Cluster
+func (c *EKS) ClusterDelete(*kingpin.ParseContext) error {
+	req := eks.CreateClusterInput{}
+	for _, deployment := range c.eksResources {
+
+		if err := yamlGo.UnmarshalStrict(deployment.Content, req); err != nil {
+			log.Fatalf("Error parsing the cluster deployment file %s:%v", deployment.FileName, err)
+		}
+
+		reqD := &eks.DeleteClusterInput{
+			Name: req.Name,
+		}
+		log.Printf("Removing cluster '%v'", reqD.Name)
+
+		// err := provider.RetryUntilTrue(
+		// 	fmt.Sprintf("deleting cluster:%v", reqD.ClusterId),
+		// 	provider.GlobalRetryCount,
+		// 	func() (bool, error) { return c.clusterDeleted(reqD) })
+
+		// if err != nil {
+		// 	log.Fatalf("removing cluster err:%v", err)
+		// }
+	}
+	return nil
+}
+
+// clusterRunning checks whether a cluster is in a active state.
+func (c *EKS) clusterRunning(name string) (bool, error) {
+	req := &eks.DescribeClusterInput{
+		Name: aws.String(name),
+	}
+	clusterRes, err := c.clientEKS.DescribeCluster(req)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == eks.ErrCodeNotFoundException {
+			return false, nil
+		}
+		return false, fmt.Errorf("Couldn't get cluster status: %v", err)
+	}
+	if *clusterRes.Cluster.Status == eks.ClusterStatusFailed {
+		return false, fmt.Errorf("Cluster not in a status to become ready - %s", *clusterRes.Cluster.Status)
+	}
+	if *clusterRes.Cluster.Status == eks.ClusterStatusActive {
+		return true, nil
+	}
+	log.Printf("Cluster '%v' status: %v", name, *clusterRes.Cluster.Status)
+	return false, nil
+}
