540fc28b9dea0e3b006d4d1173fc473a81a6385d
Kushal Shukla <kushalshukla110@gmail.com>
2024-11-07 20:28:16 +0530

    Signed-off-by: Kushal Shukla <kushalshukla110@gmail.com>
    Key changes
    1. removed multiline command and put it in script on prometheus-builder image.
    2. configure this in prometheus deployment.
    3. added download.sh script which will download the data if key present , else
    skip the downloading process and move to next container.

--- tools/prometheus-builder/key.sh
+++ tools/prometheus-builder/key.sh
@@ -2,11 +2,10 @@
 
 DIR="/go/src/github.com/prometheus/prometheus"
 
-if [[ -z $PR_NUMBER || -z $VOLUME_DIR || -z $GITHUB_ORG || -z $GITHUB_REPO ]]; then
+if [[ -z $PR_NUMBER || -z $GITHUB_ORG || -z $GITHUB_REPO ]]; then
     echo "ERROR:: environment variables not set correctly"
     exit 1;
-fi
- 
+fi 
 # Clone the repository with a shallow clone
 echo ">> Cloning repository $GITHUB_ORG/$GITHUB_REPO (shallow clone)"
 if ! git clone --depth 1 https://github.com/$GITHUB_ORG/$GITHUB_REPO.git $DIR; then
@@ -24,9 +23,9 @@ fi
 
 git checkout pr-branch
 
-# Here, MKDIR is specified in the volumeMount section of the prometheus-builder init container, 
-# where it will copy the key.yml file from the Prometheus directory to the volume section of the
-# emptyDir. This file will later be used by the data-downloader init container.
+# Here, MKDIR is specified in the volumeMount section of the download-key init container, where it 
+# will copy the key.yml file from the Prometheus directory to the volume section of the emptyDir. 
+# This file will later be used by the data-downloader init container.
 MKDIR="/config"
 if [ -f "$DIR/key.yml" ]; then
     echo "File exists."
@@ -34,12 +33,3 @@ if [ -f "$DIR/key.yml" ]; then
 else
     echo "File does not exist."
 fi
-
-echo ">> Creating prometheus binaries"
-if ! make build PROMU_BINARIES="prometheus"; then
-    echo "ERROR:: Building of binaries failed"
-    exit 1;
-fi
-
-echo ">> Copy files to volume"
-cp prometheus               $VOLUME_DIR/prometheus
